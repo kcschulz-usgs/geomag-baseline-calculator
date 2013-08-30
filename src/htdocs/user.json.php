400248a6b5a402606108f1756284fd121cbc9927
Kyle Schulz <kcschulz@usgs.gov>
2013-08-30 14:28:54 -0600

    the json feed's user and callback are both optional now

--- src/htdocs/user.json.php
+++ src/htdocs/user.json.php
@@ -3,21 +3,32 @@
 include_once '../conf/config.inc.php';
 include_once '../lib/classes/UserFactory.php';
 
-if (isset($_GET['user'])) {
-	$name = $_GET['user'];
-} else {
-	echo "A user name is required.";
-	exit();
-}
-
+$callback = null;
 if (isset($_GET['callback'])) {
 	$callback = $_GET['callback'];
-} else {
-	echo "A callback is required.";
-	exit();
 }
 
 $users = new UserFactory($DB);
-$user = $users->read($name);
 
-echo $callback . '(' . $user->getJson() . ')';
+if (isset($_GET['user'])) {
+	$name = $_GET['user'];
+} else {
+	session_start();
+	if (isset($_SESSION['username'])) {
+		$name = $_SESSION['username'];
+	} else {
+		echo "No user has been provided or is currently logged in.";
+		exit();
+	}
+}
+
+$user = $users->read($name);
+if ($user !== null) {
+	if ($callback !== null) {
+		echo $callback . '(' . $user->getJson() . ')';
+	} else {
+		echo $user->getJson();
+	}
+} else {
+	echo "No user by that username was found.";
+}
